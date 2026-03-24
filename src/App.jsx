d00d58df20d36a3caaba4bcaa977e5476188a594
0xK3rn3l-dev <emailbycat222@gmail.com>
2026-03-24 22:08:33 +0300

    fix architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import Hero from './components/Hero.jsx';
+import Hero from './sections/Hero.jsx';
 import Header from './components/Header.jsx';
-import Projects from './components/Projects.jsx';
-import Contact from './components/Contact.jsx'
+import Projects from './sections/Projects.jsx';
+import Contact from './sections/Contact.jsx'
 
 function App() {
   return (
     <div>
-
-      <>
       <Header />
-      </>
-      <div className="Hero-container">
-      <>
-      <section id="about"></section>
-      </>
-      <Hero />
-      </div>
-      
-      <br id='projects'/>
-      <br />
-      <section></section>
-      <div className='projects-container'>
-      <Projects />
-      </div>
 
-      <br id='contact'/>
-      <section></section>
-      <div className='contact-container'>
-      <Contact />
-      </div>
+      <main>
+          <div className="Hero-container">
+            <section id="about"></section>  
+              <Hero />
+          </div>
 
+          <section id='projects'></section>
+            <div className='projects-container'>
+              <Projects />
+            </div>
+
+          <section id='contact'></section>
+            <div className='contact-container'>
+              <Contact />
+            </div>
+      </main>
+    
     </div>
   )
 }
